0d7ff5c8c44591596d627e774bab62b5ef2c1909
Shivansh Singh <shivanshsingh022vns@gmail.com>
2026-02-15 18:54:23 +0530

    Update login.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -29,7 +29,7 @@ export const Login = () => {
       return handleError('All fields are required');
     }
     try {
-      const url = 'http://localhost:8080/auth/login';
+      const url = `https://authentication-app-api-henna.vercel.app/auth/login`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
